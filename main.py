6390ec7ba991dd30ca5bd55190dbc23c69ad06fb
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-03 10:20:07 +0200

    Creata struttura dei files PDF

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pandas as pd
 import glob
+from  fpdf import FPDF
+from pathlib import Path
 
 #VARIABLE
 path_input = "input_files/"
+path_output="output_files/"
 filepaths = glob.glob(path_input+"invoices/*.xlsx")
-print(filepaths)
 
-#list comprehension
 for i,filepath in enumerate(filepaths):
-    print("\n###########")
+    nome_file = Path(filepath).stem # estreggo solo il nome file senza estensione
+    numero_fattura = str(nome_file).split("-")[0]
+
+    print(f"\n\nNOME FILE: {nome_file}")
+    print(f"NUMERO_FATTURA: {numero_fattura}")
+    print("###########")
     print(f"--> filepath: {filepath}")
+# DATAFRAME
     #METODO CLASSICO
     df = pd.read_excel(filepath,sheet_name="Sheet 1")
     print(df)
     print("###########")
-
-    #METODO CON exec(). Crea dataframe con nomi diversi
-   # exec(f"df_{i+1}=pd.read_excel(filepath,sheet_name='Sheet 1')")
-   # exec(f"print(df_{i+1})")
-
-
+        #METODO CON exec(). Crea dataframe con nomi diversi
+            # exec(f"df_{i+1}=pd.read_excel(filepath,sheet_name='Sheet 1')")
+            # exec(f"print(df_{i+1})")
+#PDF FILE
+    pdf = FPDF(orientation="P" , unit = "mm", format= "A4")
+    pdf.add_page()
+    pdf.set_font(family="Times",size=16,style="B")
+    pdf.cell(w=50,h=8,txt= f"Fattura numero: {numero_fattura}")
+    pdf.output(path_output+"fattura_numero_"+nome_file+".pdf")
 
