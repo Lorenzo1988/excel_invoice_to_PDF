2b0f350eb2a8232f3b2c91be81df2d80f19d9c03
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-09 09:30:26 +0200

    Aggiunta data al documento

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ path_input = "input_files/"
 path_output="output_files/"
 filepaths = glob.glob(path_input+"invoices/*.xlsx")
 
+
 for i,filepath in enumerate(filepaths):
     nome_file = Path(filepath).stem # estreggo solo il nome file senza estensione
-    numero_fattura = str(nome_file).split("-")[0]
+    numero_fattura,data_documento = str(nome_file).split("-")
 
     print(f"\n\nNOME FILE: {nome_file}")
     print(f"NUMERO_FATTURA: {numero_fattura}")
@@ -26,8 +27,13 @@ for i,filepath in enumerate(filepaths):
             # exec(f"print(df_{i+1})")
 #PDF FILE
     pdf = FPDF(orientation="P" , unit = "mm", format= "A4")
+
     pdf.add_page()
     pdf.set_font(family="Times",size=16,style="B")
-    pdf.cell(w=50,h=8,txt= f"Fattura numero: {numero_fattura}")
+    pdf.cell(w=50,h=8,txt= f"Fattura numero: {numero_fattura}",ln=1)
+
+    pdf.set_font(family="Times",size=16,style="B")
+    pdf.cell(w=50,h=8,txt= f"Data: {data_documento}")
+
     pdf.output(path_output+"fattura_numero_"+nome_file+".pdf")
 
