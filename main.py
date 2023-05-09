b3c08d945bef13c8a4af39b782e92b0cd6ceb7d0
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-09 12:13:29 +0200

    Modifiche lettura files excel

--- main.py
+++ main.py
@@ -17,14 +17,6 @@ for i,filepath in enumerate(filepaths):
     print(f"NUMERO_FATTURA: {numero_fattura}")
     print("###########")
     print(f"--> filepath: {filepath}")
-# DATAFRAME
-    #METODO CLASSICO
-    df = pd.read_excel(filepath,sheet_name="Sheet 1")
-    print(df)
-    print("###########")
-        #METODO CON exec(). Crea dataframe con nomi diversi
-            # exec(f"df_{i+1}=pd.read_excel(filepath,sheet_name='Sheet 1')")
-            # exec(f"print(df_{i+1})")
 #PDF FILE
     pdf = FPDF(orientation="P" , unit = "mm", format= "A4")
 
@@ -35,5 +27,28 @@ for i,filepath in enumerate(filepaths):
     pdf.set_font(family="Times",size=16,style="B")
     pdf.cell(w=50,h=8,txt= f"Data: {data_documento}")
 
-    pdf.output(path_output+"fattura_numero_"+nome_file+".pdf")
 
+
+
+# DATAFRAME
+    #METODO CLASSICO
+    df = pd.read_excel(filepath,sheet_name="Sheet 1")
+    print(df)
+    print("###########")
+        #METODO CON exec(). Crea dataframe con nomi diversi
+            # exec(f"df_{i+1}=pd.read_excel(filepath,sheet_name='Sheet 1')")
+            # exec(f"print(df_{i+1})")
+
+
+    for index,row in df.iterrows():
+        print(f"\tindex: {index}")
+        print(f"\trow:  {row}")
+        pdf.set_font(family="Times",size=10,)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=8, txt=row["product_id"])
+        pdf.cell(w=70, h=8, txt=row["product_name"])
+        pdf.cell(w=30, h=8, txt=row["amount_purchased"])
+        pdf.cell(w=30, h=8, txt=row["price_per_unit"])
+        pdf.cell(w=30, h=8, txt=row["total_price"])
+
+    pdf.output(path_output + "fattura_numero_" + nome_file + ".pdf")
