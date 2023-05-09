87e5e05ee7cd5ba9efc804e32fa074b8d93104bb
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-05-09 15:29:38 +0200

    Aggiunto totale, righe dopo tabella, logo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 #VARIABLE
 path_input = "input_files/"
 path_output= "output_files/"
+path_image = "input_files/images"
 filepaths = glob.glob(path_input+"invoices/*.xlsx")
 
 for i,filepath in enumerate(filepaths):
@@ -53,12 +54,39 @@ for i,filepath in enumerate(filepaths):
     for index,row in df.iterrows():
         print(f"\tindex:{index}")
         print(f"\trow:{row}")
-        pdf.set_font(family="Times",size=12,)
+        pdf.set_font(family="Times",size=12)
         pdf.set_text_color(80,80,80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]),border=1)
         pdf.cell(w=70, h=8, txt=str(row["product_name"]),border=1)
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]),border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]),border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]),border=1,ln=1)
+
+#Aggiunta valore prezzo totale
+    somma_totale = df["total_price"].sum()
+    print(f"La somma totale è: {somma_totale}")
+
+    pdf.set_font(family="Times", size=10,style="B")
+    pdf.set_text_color(255, 0, 0)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=70, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+
+    pdf.cell(w=30, h=8, txt=str(somma_totale), border=1,ln=1)
+
+
+    #TESTO DOPO LA TABELLA
+    pdf.set_font(family="Times", size=10,style="B")
+    pdf.set_text_color(80, 80, 80)
+
+    #Total sum
+    pdf.cell(w=30, h=8, txt=f"The total price is: {somma_totale}",ln=1)
+
+    #Company Name and logo
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=60, h=8, txt=f"Company Lorenzo1988",ln=1)
+    pdf.image("input_files/images/cinghiale.png",w=20)
 
     pdf.output(path_output + "fattura_numero_" + nome_file + ".pdf")
